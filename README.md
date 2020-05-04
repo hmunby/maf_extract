ef40fa40eb67c2352db5e585ad6fcdf6307f3b6a
Hannah Munby <hmm47@cam.ac.uk>
2020-05-04 12:10:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## maf_extract
+Command-line tool to extract alignment blocks in a particular region from a Multiple Alignment File. 
+Any blocks covering the specified region are printed to standard output as they appear in the input MAF, including any commented lines. 
+### Usage
+`python ./maf_extract.py /path/to/alignment_file_to_be_searched.maf sequence_of_interest start end`
+<br /> 
+e.g. searching Ensembl 75 fish aligmnent for any blocks covering Astatotilapia calliptera chromosome 7:10,400,000-10,600,000
+<br /> 
+`python ./maf_extract.py 75_fish.epo_low_coverage.12_2.maf astatotilapia_calliptera.7 10400000 10600000`
+<br />
+
+Reference sequence for search can be any present in the alignment (i.e. doesn't have to be the same as the reference sequence for the alignment being searched), its name must be given as it appears in the MAF. 
---
+++ maf_extract.py
@@ -0,0 +1,37 @@
+import sys
+file_test = sys.argv[1]
+target_sequence_name = sys.argv[2]
+range_start = int(sys.argv[3])
+range_end = int(sys.argv[4])
+
+line_gen = open(file_test)
+reading_file = 1
+while reading_file == 1:
+    linecount = 0
+    block = []
+    while True:
+        nextline = line_gen.readline()
+        linecount = linecount + 1
+        if nextline == '\n':
+            break
+        if not nextline:
+            reading_file = 0
+            break
+        block.append(nextline)
+
+    target_line = [s for s in block if target_sequence_name in s]
+
+    if not target_line:
+        pass
+    else:
+        (start, length, direction) = target_line[0].split()[2:5]
+        if direction == '-':
+            end = int(start)
+            start = end - int(length)
+        else:
+            start = int(start)
+            end = start + int(length)
+        if (range_start <= start <= range_end) or (start <= range_start <= end):
+            print(*block)
+print("finished scanning file")
+exit
